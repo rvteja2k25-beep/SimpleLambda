34c3860491bb06a120646fcb6427869cc7cf6319
rvteja2k25-beep <rvteja2k25@gmail.com>
2025-12-01 16:36:30 +0000

    Update Function.cs

--- src/HelloLambda/Function.cs
+++ src/HelloLambda/Function.cs
@@ -16,6 +16,6 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        return input.ToUpper();
+        return "hello" + input.ToUpper();
     }
 }
